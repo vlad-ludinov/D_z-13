1d26f3bfb5338cf899a6a85575ed213ce501b1a1
vlad <vladislavludinov3006@gmail.com>
2023-01-27 18:10:40 +0300

    input parametres added

--- Task47/Program.cs
+++ Task47/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Console;
+
+Write("Введите размер массива, а также миниальное и максимальное числа через запятую: ");
+string [] parametres = ReadLine().Split(",", StringSplitOptions.RemoveEmptyEntries);
