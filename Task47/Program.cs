1b68db2623397ca1cadd38da7d96e6b5cbf4d953
vlad <vladislavludinov3006@gmail.com>
2023-01-27 18:20:33 +0300

    method creating array added

--- Task47/Program.cs
+++ Task47/Program.cs
@@ -1,4 +1,18 @@
 ﻿using static System.Console;
 
 Write("Введите размер массива, а также миниальное и максимальное числа через запятую: ");
-string [] parametres = ReadLine().Split(",", StringSplitOptions.RemoveEmptyEntries);
+string [] parametres = ReadLine()!.Split(",", StringSplitOptions.RemoveEmptyEntries);
+
+double [,] getArray(int rows, int columns, double minValue, double maxValue)
+{
+    double delta = maxValue - minValue;
+    double [,] array = new double [rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i,j] = Math.Round((((new Random().NextDouble())+(minValue/delta))*delta),1);
+        }
+    }
+    return array;
+}
